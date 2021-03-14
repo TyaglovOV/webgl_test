78a861a92b7d4a8d86b65c775b451beb93708c41
o.tyaglov <o.tyaglov@okko.tv>
2021-03-14 22:56:50 +0300

    First commit

--- src/app/shaders/vertexShader.ts
+++
@@ -1,9 +0,0 @@
-import { CanvasContext, ShaderType } from '../utils';
-import { Shader } from './shader';
-import { ShaderBase } from './shaderBase';
-
-export class VertexShader extends Shader {
-  constructor(canvasContext: CanvasContext, source: string) {
-    super(canvasContext, new ShaderBase(ShaderType.Vertex, source))
-  }
-}
